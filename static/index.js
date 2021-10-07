a859ddad16fba7ba5668e64f0568b61f98267f15
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-07 15:00:03 +0000

    made js for loading nav bar

--- static/index.js
+++ static/index.js
@@ -8,3 +8,17 @@ async function boot(){
     bootScreen = await document.getElementById("bootScreen")
     bootScreen.innerHTML += "<p>test</p>"
 }
+
+async function loadNav(){
+    navBarHTML = `
+    <div class=\"sidenav\">
+        <ul class=\"tree-view\">
+            <li><a href=\"/\">Home</a></li>
+            <li><a href=\"https://github.com/JonZavialov/portfolio2\" target=\"_blank\">Repository</a></li>
+        </ul>
+    </div>
+    `
+
+    navBar = await document.getElementById("nav")
+    navBar.innerHTML += navBarHTML
+}

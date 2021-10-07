2384fcd4ecca8b68ce6e4349c14c3b057ed3dbfc
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-07 15:06:53 +0000

    changed boot sequence

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/boot")
-def boot():
     return render_template("boot.html")
+
+@app.route("/home")
+def boot():
+    return render_template("index.html")
 
 app.run(host='0.0.0.0',port=80)
--- static/index.js
+++ static/index.js
@@ -1,9 +1,5 @@
 //Jonathan Zavialov
 
-async function replaceBootScreen(){
-    window.location.replace("/boot")
-}
-
 async function boot(){
     bootScreen = await document.getElementById("bootScreen")
     bootScreen.innerHTML += "<p>test</p>"
@@ -13,7 +9,7 @@ async function loadNav(){
     navBarHTML = `
     <div class=\"sidenav\">
         <ul class=\"tree-view\">
-            <li><a href=\"/\">Home</a></li>
+            <li><a href=\"/home\">Home</a></li>
             <li><a href=\"https://github.com/JonZavialov/portfolio2\" target=\"_blank\">Repository</a></li>
         </ul>
     </div>

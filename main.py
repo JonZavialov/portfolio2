e1ae0450b89e0b2aef2123b04c53d8d74655f898
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-07 16:13:44 +0000

    added 404 route and page- will style later

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ def home():
 def boot():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 app.run(host='0.0.0.0',port=80)

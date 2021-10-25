70f845136b5e92b000e5450d5045908444e2eca4
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-25 11:27:52 -0400

    began creating start menu

---
+++ static/js/startMenu.js
@@ -0,0 +1,6 @@
+async function startMenu(){
+    let main = document.getElementById("main")
+    let element = document.createElement("div")
+    element.id = "startMenu"
+    main.appendChild(element)
+}

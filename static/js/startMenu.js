ec67cdb0858adca89e2a1d277231e82ccc382302
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-27 23:45:07 -0400

    added close cases for startmenu

--- static/js/startMenu.js
+++ static/js/startMenu.js
@@ -1,19 +1,38 @@
 async function startMenu(){
+    console.log('creating start menu')
+    //await sleep(.01)
+    if(document.getElementById("startMenu")) return
     let main = document.getElementById("main")
     let element = document.createElement("div")
     element.id = "startMenu"
     element.className = "window"
     element.innerHTML = `
-    <div class="window-body">
-        <div class="title-bar">
-            <div class="title-bar-text">
-                Windows98
-            </div>
+    <div class="title-bar">
+        <div class="title-bar-text">
+            Windows98
         </div>
-        <div id="startMenuBody">
-            <p>Souse</p>
-        </div>
+    </div>
+    <div id="startMenuBody">
+        <p>Souse</p>
     </div>
     `
     main.appendChild(element)
+
+    let startButton = document.getElementById("startButton")
+    startButton.onclick =function() { closeButton() }
+}
+
+async function startMenuListener(){
+    main.onclick = function (e) {
+        if(e.target.closest("#startMenu") || !document.getElementById("startMenu") || e.target.closest("#startButton")) return
+        console.log('clicked main')
+        closeButton()
+    }
+}
+
+async function closeButton(){
+    console.log('deleting start menu')
+    document.getElementById("startMenu").remove()
+    let startButton = document.getElementById("startButton")
+    startButton.onclick =function() { startMenu() }
 }

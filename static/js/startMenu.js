362ac9274ad82a4caa89f0bf7ab6c00bc475da33
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-28 21:51:18 -0400

    sync

--- static/js/startMenu.js
+++ static/js/startMenu.js
@@ -13,39 +13,39 @@ async function startMenu(){
         </div>
     </div>
     <div id="startMenuBody">
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('email')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/mail.png?raw=true">
             <p id="startMenuPairText">Email Me</p>
             <p id="startMenuArrow">&#9654</p>
         </div>
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('linkedin')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/linkedin.png?raw=true">
             <p id="startMenuPairText">Linkedin</p>
             <p id="startMenuArrow">&#9654</p>
         </div>
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('insta')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/instagram.png?raw=true">
             <p id="startMenuPairText">Instagram</p>
             <p id="startMenuArrow">&#9654</p>
         </div>
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('youtube')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/youtube.png?raw=true">
             <p id="startMenuPairText">Youtube</p>
             <p id="startMenuArrow">&#9654</p>
         </div>
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('github')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/documents.png?raw=true">
             <p id="startMenuPairText">GitHub</p>
             <p id="startMenuArrow">&#9654</p>
         </div>
         <hr>
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('credits')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/credits.png?raw=true">
             <p id="startMenuPairText">Credits</p>
             <p id="startMenuArrow">&#9654</p>
         </div>
         <hr>
-        <div class="hoverHighlight" id="startMenuPair">
+        <div class="hoverHighlight" id="startMenuPair" onclick="startMenuButton('shutdown')">
             <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/shutdown.png?raw=true">
             <p id="startMenuPairText">Shut Down...</p>
             <p id="startMenuArrow">&#9654</p>
@@ -63,4 +63,46 @@ async function closeButton(){
     document.getElementById("startMenu").remove()
     let startButton = document.getElementById("startButton")
     startButton.onclick =function() { startMenu() }
+}
+
+async function startMenuButton(button){
+    let methods = {
+        "email": emailButton(),
+        "linkedin": linkedin(),
+        "insta": insta(),
+        "youtube": youtube(),
+        "github": github(),
+        "credits": credits(),
+        "shutdown": shutdown()
+    }
+    let methodNames = Object.keys(methods)
+    if(methodNames.indexOf(button) != -1) methods[button]
+}
+
+async function emailButton(){
+    console.log('email button pressed')
+}
+
+async function linkedin(){
+    console.log('linkedin button pressed')
+}
+
+async function insta(){
+    console.log('insta button pressed')
+}
+
+async function youtube(){
+    console.log('youtube button pressed')
+}
+
+async function github(){
+    console.log('github button pressed')
+}
+
+async function credits(){
+    console.log('credits button pressed')
+}
+
+async function shutdown(){
+    console.log('shutdown button pressed')
 }

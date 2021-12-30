dd2b2ba845eb9df48ba31a95ed8b6ac63678378c
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-12-29 21:39:04 -0500

    added inboxicon image

--- static/js/email.js
+++ static/js/email.js
@@ -16,9 +16,31 @@ async function initEmail(documentId = null){
                     <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/images/address.png?raw=true">
                     <p>Address Book</p>
                 </div>
+                <hr id="outlookHr3">
+                <div id="outlookHeaderButton" style="margin-left: 10px; transform:translate(0px, -8px)">
+                    <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/images/connect.png?raw=true">
+                    <p>Connect</p>
+                </div>
+                <div id="outlookHeaderButton" style="margin-right: 100px; transform:translate(0px, -8px)">
+                    <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/images/hangup.png?raw=true">
+                    <p>Hang Up</p>
+                </div>
+                <hr id="outlookHr4">
+                <div id="outlookHeaderLogo">
+                    <img src="https://raw.githubusercontent.com/JonZavialov/explorersvg/main/iesvg.svg">
+                </div>
             </div>
+
+            <ul class="tree-view" id="inboxTree">
+                CSS
+                <ul>
+                    <li>Selectors</li>
+                    <li>Specificity</li>
+                    <li>Properties</li>
+                </ul>
+            </ul>
         </div>
     `
-    openWindow(content,"email","<img width=13px src=\"https://github.com/JonZavialov/portfolio2/blob/main/assets/images/outlook.png?raw=true\">&nbsp&nbspOutlook Express",[300,250],true)
+    openWindow(content,"email","<img width=13px src=\"https://github.com/JonZavialov/portfolio2/blob/main/assets/images/outlook.png?raw=true\">&nbsp&nbspOutlook Express",[50,250],true)
     taskbarUpdate("https://github.com/JonZavialov/portfolio2/blob/main/assets/images/outlook.png?raw=true","Outlook Express","email")
 }

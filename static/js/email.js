f8754dd67e645f52868cc8665691e8916c502063
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-12-29 20:26:21 -0500

    added mailbox image

--- static/js/email.js
+++ static/js/email.js
@@ -1,7 +1,17 @@
 async function initEmail(documentId = null){
     let content = `
         <div id="emailBody">
-            email
+            <div id="buttonsHeader">
+                <div id="outlookHeaderButton">
+                    <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/images/compose.png?raw=true">
+                    <p>Compose Message</p>
+                </div>
+                <hr id="outlookHr1">
+                <div id="outlookHeaderButton">
+                    <img src="https://github.com/JonZavialov/portfolio2/blob/main/assets/images/mailbox.png?raw=true">
+                    <p>Send And Receive</p>
+                </div>
+            </div>
         </div>
     `
     openWindow(content,"email","<img width=13px src=\"https://github.com/JonZavialov/portfolio2/blob/main/assets/images/outlook.png?raw=true\">&nbsp&nbspOutlook Express",[300,250],true)
